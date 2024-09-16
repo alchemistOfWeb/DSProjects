6e0b14e68e6ec82c4f0132a177a73a10a5531241
Alchemist of the web <nikitakzntcv@gmail.com>
2024-09-16 10:15:32 +0400

    test1 passed

--- Vector/Tests/1BasicConstructorsTest.h
+++ Vector/Tests/1BasicConstructorsTest.h
@@ -4,19 +4,19 @@
 
 namespace test
 {
-  struct BasicConstructorsTest
-  {
-    BasicConstructorsTest()
+    struct BasicConstructorsTest
     {
-  	  Vector<int> vec1;
-  	  Vector<double> vec2(10);
-  	  Vector<char> vec3(30, 'x');
+        BasicConstructorsTest()
+        {
+  	        Vector<int> vec1;
+  	        Vector<double> vec2(10);
+  	        Vector<char> vec3(30, 'x');
   	  
-  	  assertEqual(vec1.size(), 0, __LINE__, __FILE__);
-  	  assertEqual(vec2.size(), 10, __LINE__, __FILE__);
-  	  assertEqual(vec3.size(), 30, __LINE__, __FILE__);
-    }
-  };
+  	        assertEqual(vec1.size(), 0, __LINE__, __FILE__);
+  	        assertEqual(vec2.size(), 10, __LINE__, __FILE__);
+  	        assertEqual(vec3.size(), 30, __LINE__, __FILE__);
+        }
+    };
   
-  static BasicConstructorsTest basicConstructorsTest;
+    static BasicConstructorsTest basicConstructorsTest;
 }
--- Vector/Vector.h
+++ Vector/Vector.h
@@ -2,8 +2,43 @@
 // given in main.cpp.
 
 // Again: Declare your functions inside your class, but define them outside of it.
+//
+//#include <vector>
+//
+//template <typename T>
+//using Vector = std::vector<T>;
 
-#include <vector>
+
+#include <initializer_list> 
+
 
 template <typename T>
-using Vector = std::vector<T>;
+class Vector {
+private:
+
+    T* m_arr;
+    std::size_t m_size; // number of elements
+    std::size_t m_capacity; // real number of array elements in memory
+
+public:
+    //Vector() : m_arr(new T[3]), m_size(0), m_capacity(3) {}
+    Vector(std::size_t size=0): m_arr(new T[size]), m_size(0), m_capacity(size) {}
+
+    Vector(std::size_t size, T filler): m_arr(new T[size]), m_size(size), m_capacity(size) {
+        for (size_t i = 0; i < m_size; i++) {
+            m_arr[i] = filler;
+        }
+    }
+
+    Vector(const std::initializer_list<T>& init_list) {
+
+    }
+
+    size_t size() {
+        return m_capacity;
+    }
+
+    ~Vector() {
+
+    }
+};
