be8cc12f9812d152167f08f9e3c65162a56d454e
Alchemist of the web <nikitakzntcv@gmail.com>
2024-09-18 12:10:41 +0400

    vector: test12 passed; some fixes;

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -10,32 +10,28 @@
 //using Vector = std::vector<T>;
 
 
+#include <stdexcept>
 #include <initializer_list> 
+#include <iostream>
 
 
 template <class T>
 class Vector {
-private:
-    T* m_arr = nullptr;
-    std::size_t m_size; // number of elements
-    std::size_t m_capacity; // real number of array elements in memory
-
 public:
-    //Vector() : m_arr(new T[3]), m_size(0), m_capacity(3) {}
-    //Vector(std::size_t size=0): m_arr(new T[size]), m_size(0), m_capacity(size) {}
     
-    Vector(std::size_t size=0): m_arr(new T[size]), m_size(size), m_capacity(size) {
+    Vector(std::size_t size=0): m_size(size), m_capacity(size) {
+        m_arr = new T[size];
         for (size_t i = 0; i < m_size; i++) {
             m_arr[i] = T();
         }
     }
 
-    Vector(std::size_t size, T filler): m_arr(new T[size]), m_size(size), m_capacity(size) {
+    Vector(std::size_t size, T filler): m_size(size), m_capacity(size) {
+        m_arr = new T[size];
         for (size_t i = 0; i < m_size; i++) {
             m_arr[i] = filler;
         }
     }
-
 
     Vector(const std::initializer_list<T>& init_list) {
         m_size = m_capacity = init_list.size();
@@ -59,6 +55,10 @@ public:
         m_arr = newarr;
     }
 
+    //Vector(Vector<T>&& other) : m_arr(), m_size(other.m_size), m_capacity(other.m_capacity) {
+    //    
+    //}
+
     size_t size() {
         return m_size;
     }
@@ -71,9 +71,10 @@ public:
         return m_arr[index];
     }
 
-    T& at(const size_t& index) {
-        // TODO: Add check 
-        return m_arr[index];
+    T& at(const size_t& pos) {
+        if (pos >= m_size)
+            throw std::invalid_argument("value is out of bounce in at method");
+        return m_arr[pos];
     }
 
     T& front() {
@@ -91,12 +92,7 @@ public:
 
     void push_back(T value) {
         if (m_size == m_capacity) {
-            T* newarr = new T[m_capacity += 3];
-            for (size_t i = 0; i < m_size; i++) {
-                newarr[i] = m_arr[i];
-            }
-            delete[] m_arr;
-            m_arr = newarr;
+            reallocate(m_capacity += 3);
         }
         m_arr[m_size++] = value;
     }
@@ -105,8 +101,14 @@ public:
         return m_arr[m_size--];
     }
 
-    void reserve(size_t capacity) {
-        m_capacity = capacity;
+    //T& emplace_back(T& args...) {
+    //    m_arr[m_size++] = T(args...);
+    //    reutrn m_arr[m_size - 1];
+    //}
+
+    void reserve(size_t new_cap) {
+        if (new_cap <= m_capacity) return;
+        reallocate(m_capacity = new_cap);
     }
 
     size_t capacity() {
@@ -114,10 +116,8 @@ public:
     }
 
     void shrink_to_fit() {
-        m_capacity = m_size;
+        reallocate(m_capacity = m_size);
     }
-
-
 
     void clear() {
         m_size = 0;
@@ -128,7 +128,6 @@ public:
     }
 
     void swap(Vector& other) {
-        // Need deep or not deep copy?
         T* tmparr = m_arr;
         size_t tmpsize = m_size;
         size_t tmpcap = m_capacity;
@@ -188,4 +187,22 @@ public:
     ~Vector() {
         delete[] m_arr;
     }
+
+private:
+    T* m_arr = nullptr;
+    std::size_t m_size; // number of elements
+    std::size_t m_capacity; // real number of array elements in memory
+
+    void reallocate(size_t new_capacity) {
+        if (new_capacity <= m_size) return;
+        T* newarr = new T[new_capacity];
+        for (size_t i = 0; i < m_size; i++) {
+            newarr[i] = std::move(m_arr[i]);
+            m_arr[i] = T();
+        }
+
+        delete[] m_arr;
+        m_arr = newarr;
+    }
+
 };
