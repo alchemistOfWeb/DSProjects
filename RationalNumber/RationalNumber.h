ee6f388a7aaab7dde7c99f714ee1b511051211d2
Alchemist of the web <nikitakzntcv@gmail.com>
2024-09-21 20:20:45 +0400

    Separate method definitions from RationalNumber class declarations

--- RationalNumber/RationalNumber.h
+++ RationalNumber/RationalNumber.h
@@ -54,267 +54,106 @@ namespace numbers
 			m_denominator /= gcd;
 		}
 	public:
-		RationalNumber(T numerator = 0, T denominator = 1) {
-			// TODO: simplify or optimize
-		
-			if (numerator == denominator) {
-				numerator = denominator = 1;
-			} else {
-				int gcd = calculate_gcd(numerator, denominator);
-				if (gcd != 1) {
-					numerator /= gcd;
-					denominator /= gcd;
-				}
-			}
-			if (numerator < 0 && denominator < 0) {
-				numerator = -(signed)numerator;
-				denominator = -(signed)denominator;
-			}
-			if (denominator < 0) {
-				numerator = -(signed)numerator;
-				denominator = -(signed)denominator;
-			}
+		RationalNumber(T numerator = 0, T denominator = 1);
 
-			m_numerator = numerator;
-			m_denominator = denominator;
-		}
+		RationalNumber(long double& other);
 
-		RationalNumber(long double& other) {
-			m_denominator = other.m_denominator;
-			m_numerator = other.m_numerator;
-		}
-
-		RationalNumber(RationalNumber<T>& other) {
-			m_denominator = other.m_denominator;
-			m_numerator = other.m_numerator;
-		}
-	
-	
-		RationalNumber& operator*=(RationalNumber<T>& other) {
-			m_numerator = m_numerator * other.m_numerator;
-			m_denominator = m_denominator * other.m_denominator;
-			this->simplify();
-			return *this;
-		}
-
-		RationalNumber& operator/=(RationalNumber<T>& other) {
-			m_numerator = m_numerator * other.m_denominator;
-			m_denominator = m_denominator * other.m_numerator;
-			this->simplify();
-			return *this;
-		}
+		RationalNumber(RationalNumber<T>& other);
 	
 
 		// Plus--------------------------------------:
-		RationalNumber<T> operator+(RationalNumber<T>& other) {
-			T outerMultiplyier = calculate_gcd(m_numerator, other.m_numerator);
-			T outerDivisor = calculate_gcd(m_denominator, other.m_denominator);
+		RationalNumber<T> operator+(RationalNumber<T>& other);
 
-			T left_denominator = (other.m_denominator / outerDivisor);
-			T right_denominator = (m_denominator / outerDivisor);
+		template<IntegralValue T>
+		friend RationalNumber<T> operator+(T left, const RationalNumber<T>& right);
 
-			T left_numerator = (m_numerator / outerMultiplyier) * left_denominator;
-			T right_numerator = (other.m_numerator / outerMultiplyier) * right_denominator;
-			T numerator = left_numerator + right_numerator;
-			T denominator = left_denominator * right_denominator;
+		template<IntegralValue T>
+		friend RationalNumber<T> operator+(const RationalNumber<T>& left, T right);
 
-			return RationalNumber<T>(outerMultiplyier * numerator, denominator * outerDivisor);
-		}
+		RationalNumber<T> operator+();
 
-		friend RationalNumber<T> operator+(T left, RationalNumber<T>& right) {
-			T numerator = left * right.m_denominator + right.m_numerator;
-			return RationalNumber(numerator, right.m_denominator);
-		}
+		RationalNumber<T>& operator+=(RationalNumber& other);
 
-		friend RationalNumber<T> operator+(RationalNumber<T>& left, T right) {
-			return right + left;
-		}
+		RationalNumber<T>& operator+=(int& other);
 
+		RationalNumber<T>& operator++();
 
-		RationalNumber<T> operator+()
-		{
-			return RationalNumber<T>(+m_numerator, +m_denominator);
-		}
+		RationalNumber<T> operator++(int);
 
-		RationalNumber<T>& operator+=(RationalNumber& other)
-		{
-			*this = *this + other;
-			return *this;
-		}
-
-		RationalNumber<T>& operator+=(int& other)
-		{
-			*this = *this + other;
-			return *this;
-		}
-
-		RationalNumber& operator++()
-		{
-			m_numerator += m_denominator;
-			return *this;
-		}
-
-		RationalNumber<T> operator++(int)
-		{
-			RationalNumber<T> old_value = *this;
-			m_numerator += m_denominator;
-			return old_value;
-		}
 
 		// Minus--------------------------------------:
-		RationalNumber<T> operator-(RationalNumber<T>& other)
-		{
-			T numerator = m_numerator * other.m_denominator - other.m_numerator * m_denominator;
-			T denominator = m_denominator * other.m_denominator;
-			return RationalNumber<T>(numerator, denominator);
-		}
+		RationalNumber<T> operator-(RationalNumber<T>& other);
 
-		friend RationalNumber<T> operator-(RationalNumber<T>& left, int right) {
-			T numerator = left.m_numerator - right * left.m_denominator;
-			return RationalNumber<T>(numerator, left.m_denominator);
-		}
+		template<IntegralValue T>
+		friend RationalNumber<T> operator-(RationalNumber<T>& left, int right);
 
-		friend RationalNumber<T> operator-(int left, RationalNumber<T>& right) {
-			T numerator = left * right.m_denominator - right.m_numerator;
-			return RationalNumber<T>(numerator, right.m_denominator);
-		}
+		template<IntegralValue T>
+		friend RationalNumber<T> operator-(int left, RationalNumber<T>& right);
 
-		RationalNumber<T> operator-() {
-			return RationalNumber<T>(-m_numerator, m_denominator);
-		}
+		RationalNumber<T> operator-();
 
-		RationalNumber<T>& operator-=(RationalNumber<T>& other) {
-			*this = *this - other;
-			return *this;
-		}
+		RationalNumber<T>& operator-=(RationalNumber<T>& other);
 
-		RationalNumber<T>& operator-=(int& other) {
-			return *this - other;
-		}
+		RationalNumber<T>& operator-=(int& other);
 
-		RationalNumber& operator--() {
-			return *this -= 1;
-		}
-		RationalNumber operator--(int) {
-			return;
-		}
+		RationalNumber<T>& operator--();
+
+		RationalNumber<T> operator--(int);
+
 
 		// Multiplying--------------------------------------:
-		RationalNumber<T> operator*(RationalNumber<T>& other) {
-			// from left up to right down
-			T leftRightGCD = calculate_gcd(m_numerator, other.m_denominator);
-			// from right up to left down
-			T rightLeftGCD = calculate_gcd(m_denominator, other.m_numerator);
+		RationalNumber<T> operator*(RationalNumber<T>& other);
 
-			T left_numerator = m_numerator / leftRightGCD;
-			T right_numerator = other.m_numerator / rightLeftGCD;
+		RationalNumber& operator*=(RationalNumber<T>& other);
 
-			T left_denominator = m_denominator / rightLeftGCD;
-			T right_denominator = other.m_denominator / leftRightGCD;
+		template<IntegralValue T>
+		friend RationalNumber<T> operator*(RationalNumber<T>& left, int right);
 
-			return RationalNumber<T>(
-				left_numerator * right_numerator,
-				left_denominator * right_denominator
-			);
-		}
+		template<IntegralValue T>
+		friend RationalNumber<T> operator*(int left, RationalNumber<T>& right);
 
-		friend RationalNumber<T> operator*(RationalNumber<T>& left, int right) {
-			return RationalNumber<T>(
-				left.m_numerator * right,
-				left.m_denominator
-			);
-		}
-
-		friend RationalNumber<T> operator*(int left, RationalNumber<T>& right) {
-			return RationalNumber<T>(
-				right.m_numerator * left,
-				right.m_denominator
-			);
-		}
 
 		// Dividing--------------------------------------:
-		RationalNumber<T> operator/(RationalNumber<T>& other) {
-			return RationalNumber<T>(
-				m_numerator * other.m_denominator,
-				m_denominator * other.m_numerator
-			);
-		}
+		RationalNumber<T> operator/(RationalNumber<T>& other);
+
+		RationalNumber& operator/=(RationalNumber<T>& other);
 	
-		RationalNumber<T> operator/(const RationalNumber<T>& other) const {
-			return RationalNumber<T>(
-				m_numerator * other.m_denominator,
-				m_denominator * other.m_numerator
-			);
-		}
+		RationalNumber<T> operator/(const RationalNumber<T>& other) const;
 
-		friend RationalNumber<T> operator/(RationalNumber<T>& left, T right) {
-			return RationalNumber<T>(
-				left.m_numerator,
-				left.m_denominator * right
-			);
-		}
+		template<IntegralValue T>
+		friend RationalNumber<T> operator/(RationalNumber<T>& left, T right);
 
-		friend RationalNumber<T> operator/(T left, RationalNumber<T>& right) {
-			return RationalNumber<T>(
-				right.m_numerator,
-				right.m_denominator * left
-			);
-		}
+		template<IntegralValue T>
+		friend RationalNumber<T> operator/(T left, RationalNumber<T>& right);
 
 		// Input & Output--------------------------------------
-		friend std::ostream& operator<<(std::ostream& out, const RationalNumber<T>& number) {
-			return out << number.m_numerator << '/' << number.m_denominator;
-		}
+		template<IntegralValue T>
+		friend std::ostream& operator<<(std::ostream& out, const RationalNumber<T>& number);
 
-		friend std::istream& operator>>(std::istream& in, RationalNumber<T>& number) {
-			T a, b;
-			in >> a >> b;
-			number.m_numerator = a;
-			number.m_denominator = a;
-			return in;
-		}
+		template<IntegralValue T>
+		friend std::istream& operator>>(std::istream& in, RationalNumber<T>& number);
 	
 		// Comparison operators--------------------------------------:
-		bool operator>(RationalNumber& other) {
-			return (m_numerator * other.m_denominator) > (other.m_numerator * m_denominator);
-		}
+		bool operator>(RationalNumber<T>& other);
 
-		bool operator<(RationalNumber& other) {
-			return (m_numerator * other.m_denominator) < (other.m_numerator * m_denominator);
-		}
+		bool operator<(RationalNumber<T>& other);
 
-		bool operator==(RationalNumber& other) {
-			return (m_numerator * other.m_denominator) == (other.m_numerator * m_denominator);
-		}
+		bool operator==(RationalNumber<T>& other);
 
-		bool operator!=(RationalNumber& other) {
-			return (m_numerator * other.m_denominator) != (other.m_numerator * m_denominator);
-		}
+		bool operator!=(RationalNumber<T>& other);
 
-		bool operator!=(const RationalNumber& other) const {
-			return (m_numerator * other.m_denominator) != (other.m_numerator * m_denominator);
-		}
+		bool operator!=(const RationalNumber<T>& other) const;
 
-		bool operator>=(RationalNumber& other) {
-			return (m_numerator * other.m_denominator) >= (other.m_numerator * m_denominator);
-		}
+		bool operator>=(RationalNumber<T>& other);
 
-		bool operator<=(RationalNumber & other) {
-			return (m_numerator * other.m_denominator) <= (other.m_numerator * m_denominator);
-		}
+		bool operator<=(RationalNumber<T>& other);
 
 		// Type conversion operators--------------------------------------:
-		operator int() {
-			return m_numerator / m_denominator;
-		};
+		operator int();
 
-		operator float() {
-			return (float)m_numerator / m_denominator;
-		};
+		operator float();
 
-		operator double() {
-			return (double)m_numerator / m_denominator;
-		};
+		operator double();
 
 	}; // class RationalNumber
 	
@@ -323,6 +162,311 @@ namespace numbers
 			return RationalNumber<unsigned long long>(value, 1);
 		}
 	}
+
+
+	// RationalNumber methods implementations:
+
+	template<IntegralValue T>
+	RationalNumber<T>::RationalNumber(T numerator, T denominator) {
+		// TODO: simplify or optimize
+
+		if (numerator == denominator) {
+			numerator = denominator = 1;
+		}
+		else {
+			int gcd = calculate_gcd<T>(numerator, denominator);
+			if (gcd != 1) {
+				numerator /= gcd;
+				denominator /= gcd;
+			}
+		}
+		if (numerator < 0 && denominator < 0) {
+			numerator = -(signed)numerator;
+			denominator = -(signed)denominator;
+		}
+		if (denominator < 0) {
+			numerator = -(signed)numerator;
+			denominator = -(signed)denominator;
+		}
+
+		m_numerator = numerator;
+		m_denominator = denominator;
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T>::RationalNumber(long double& other) {
+		m_denominator = other.m_denominator;
+		m_numerator = other.m_numerator;
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T>::RationalNumber(RationalNumber<T>& other) {
+		m_denominator = other.m_denominator;
+		m_numerator = other.m_numerator;
+	}
+
+
+	// Plus--------------------------------------:
+	template<IntegralValue T>
+	RationalNumber<T> RationalNumber<T>::operator+(RationalNumber<T>& other) {
+		T outerMultiplyier = calculate_gcd(m_numerator, other.m_numerator);
+		T outerDivisor = calculate_gcd(m_denominator, other.m_denominator);
+
+		T left_denominator = (other.m_denominator / outerDivisor);
+		T right_denominator = (m_denominator / outerDivisor);
+
+		T left_numerator = (m_numerator / outerMultiplyier) * left_denominator;
+		T right_numerator = (other.m_numerator / outerMultiplyier) * right_denominator;
+		T numerator = left_numerator + right_numerator;
+		T denominator = left_denominator * right_denominator;
+
+		return RationalNumber<T>(outerMultiplyier * numerator, denominator * outerDivisor);
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T> operator+(T left, const RationalNumber<T>& right) {
+		T numerator = left * right.m_denominator + right.m_numerator;
+		return RationalNumber<T>(numerator, right.m_denominator);
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T> operator+(const RationalNumber<T>& left, T right) {
+		return right + left;
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T> RationalNumber<T>::operator+() {
+		return RationalNumber<T>(+m_numerator, +m_denominator);
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T>& RationalNumber<T>::operator+=(RationalNumber& other) {
+		return *this = *this + other;
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T>& RationalNumber<T>::operator+=(int& other) {
+		return *this = *this + other;
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T>& RationalNumber<T>::operator++() {
+		m_numerator += m_denominator;
+		return *this;
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T> RationalNumber<T>::operator++(int) {
+		RationalNumber<T> old_value = *this;
+		m_numerator += m_denominator;
+		return old_value;
+	}
+
+	// Minus--------------------------------------:
+
+	template<IntegralValue T>
+	RationalNumber<T> RationalNumber<T>::operator-(RationalNumber<T>& other)
+	{
+		T numerator = m_numerator * other.m_denominator - other.m_numerator * m_denominator;
+		T denominator = m_denominator * other.m_denominator;
+		return RationalNumber<T>(numerator, denominator);
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T> operator-(RationalNumber<T>& left, int right) {
+		T numerator = left.m_numerator - right * left.m_denominator;
+		return RationalNumber<T>(numerator, left.m_denominator);
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T> operator-(int left, RationalNumber<T>& right) {
+		T numerator = left * right.m_denominator - right.m_numerator;
+		return RationalNumber<T>(numerator, right.m_denominator);
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T> RationalNumber<T>::operator-() {
+		return RationalNumber<T>(-m_numerator, m_denominator);
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T>& RationalNumber<T>::operator-=(RationalNumber<T>& other) {
+		*this = *this - other;
+		return *this;
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T>& RationalNumber<T>::operator-=(int& other) {
+		return *this - other;
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T>& RationalNumber<T>::operator--() {
+		return *this -= 1;
+	}
+	
+	template<IntegralValue T>
+	RationalNumber<T> RationalNumber<T>::operator--(int) {
+		RationalNumber<T> old_value = *this;
+		m_numerator -= m_denominator;
+		return old_value;
+	}
+
+	// Multiplying--------------------------------------:
+	template<IntegralValue T>
+	RationalNumber<T> RationalNumber<T>::operator*(RationalNumber<T>& other) {
+		// from left up to right down
+		T leftRightGCD = calculate_gcd(m_numerator, other.m_denominator);
+		// from right up to left down
+		T rightLeftGCD = calculate_gcd(m_denominator, other.m_numerator);
+
+		T left_numerator = m_numerator / leftRightGCD;
+		T right_numerator = other.m_numerator / rightLeftGCD;
+
+		T left_denominator = m_denominator / rightLeftGCD;
+		T right_denominator = other.m_denominator / leftRightGCD;
+
+		return RationalNumber<T>(
+			left_numerator * right_numerator,
+			left_denominator * right_denominator
+		);
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T>& RationalNumber<T>::operator*=(RationalNumber<T>& other) {
+		m_numerator = m_numerator * other.m_numerator;
+		m_denominator = m_denominator * other.m_denominator;
+		this->simplify();
+		return *this;
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T> operator*(RationalNumber<T>& left, int right) {
+		return RationalNumber<T>(
+			left.m_numerator * right,
+			left.m_denominator
+		);
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T> operator*(int left, RationalNumber<T>& right) {
+		return RationalNumber<T>(
+			right.m_numerator * left,
+			right.m_denominator
+		);
+	}
+
+	// Dividing--------------------------------------:
+	template<IntegralValue T>
+	RationalNumber<T> RationalNumber<T>::operator/(RationalNumber<T>& other) {
+		return RationalNumber<T>(
+			m_numerator * other.m_denominator,
+			m_denominator * other.m_numerator
+		);
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T>& RationalNumber<T>::operator/=(RationalNumber<T>& other) {
+		m_numerator = m_numerator * other.m_denominator;
+		m_denominator = m_denominator * other.m_numerator;
+		this->simplify();
+		return *this;
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T> RationalNumber<T>::operator/(const RationalNumber<T>& other) const {
+		return RationalNumber<T>(
+			m_numerator * other.m_denominator,
+			m_denominator * other.m_numerator
+		);
+	}
+
+
+	template<IntegralValue T>
+	RationalNumber<T> operator/(RationalNumber<T>& left, T right) {
+		return RationalNumber<T>(
+			left.m_numerator,
+			left.m_denominator * right
+		);
+	}
+
+	template<IntegralValue T>
+	RationalNumber<T> operator/(T left, RationalNumber<T>& right) {
+		return RationalNumber<T>(
+			right.m_numerator,
+			right.m_denominator * left
+		);
+	}
+
+
+	// Input & Output--------------------------------------
+	template<IntegralValue T>
+	std::ostream& operator<<(std::ostream& out, const RationalNumber<T>& number) {
+		return out << number.m_numerator << '/' << number.m_denominator;
+	}
+
+	template<IntegralValue T>
+	std::istream& operator>>(std::istream& in, RationalNumber<T>& number) {
+		T a, b;
+		in >> a >> b;
+		number.m_numerator = a;
+		number.m_denominator = a;
+		return in;
+	}
+
+
+	// Comparison operators--------------------------------------:
+	
+	template<IntegralValue T>
+	bool RationalNumber<T>::operator>(RationalNumber<T>& other) {
+		return (m_numerator * other.m_denominator) > (other.m_numerator * m_denominator);
+	}
+
+	template<IntegralValue T>
+	bool RationalNumber<T>::operator<(RationalNumber<T>& other) {
+		return (m_numerator * other.m_denominator) < (other.m_numerator * m_denominator);
+	}
+
+	template<IntegralValue T>
+	bool RationalNumber<T>::operator==(RationalNumber<T>& other) {
+		return (m_numerator * other.m_denominator) == (other.m_numerator * m_denominator);
+	}
+	
+	template<IntegralValue T>
+	bool RationalNumber<T>::operator!=(RationalNumber<T>& other) {
+		return (m_numerator * other.m_denominator) != (other.m_numerator * m_denominator);
+	}
+
+	template<IntegralValue T>
+	bool RationalNumber<T>::operator!=(const RationalNumber<T>& other) const {
+		return (m_numerator * other.m_denominator) != (other.m_numerator * m_denominator);
+	}
+
+	template<IntegralValue T>
+	bool RationalNumber<T>::operator>=(RationalNumber<T>& other) {
+		return (m_numerator * other.m_denominator) >= (other.m_numerator * m_denominator);
+	}
+	
+	template<IntegralValue T>
+	bool RationalNumber<T>::operator<=(RationalNumber<T>& other) {
+		return (m_numerator * other.m_denominator) <= (other.m_numerator * m_denominator);
+	}
+
+	// Type conversion operators--------------------------------------:
+	template<IntegralValue T>
+	RationalNumber<T>::operator int() {
+		return m_numerator / m_denominator;
+	};
+
+	template<IntegralValue T>
+	RationalNumber<T>::operator float() {
+		return (float)m_numerator / m_denominator;
+	};
+
+	template<IntegralValue T>
+	RationalNumber<T>::operator double() {
+		return (double)m_numerator / m_denominator;
+	};
 
 } // namespace numbers
 
