e98cf11fa22292dfdf3be9ce82d5c367ad5d6719
Alchemist of the web <nikitakzntcv@gmail.com>
2024-11-22 14:24:48 +0400

    set: test 3 passed

--- Set/Set.cpp
+++ Set/Set.cpp
@@ -45,13 +45,58 @@ void Set::insert(int value) {
             continue;
         }
     }
-    *current = new TreeNode(value); // *current = or **current =???
+    *current = new TreeNode(value); 
     m_size++;
     return;
 }
 
 void Set::erase(int value) {
+    TreeNode** current = &m_treeRoot;
 
+    // look for the node
+    while (true) {
+        if (value == (*current)->m_value) break;
+        if (value > (*current)->m_value) {
+            current = &(*current)->m_right;
+            continue;
+        }
+        if (value < (*current)->m_value) {
+            current = &(*current)->m_left;
+            continue;
+        }
+        if (!(*current)) return;
+    }
+    bool hasRight = (*current)->m_right != nullptr;
+    bool hasLeft = (*current)->m_left != nullptr;
+
+    if (!hasLeft && hasRight) { // has only right
+        TreeNode* tmp = (*current)->m_right;
+        (*current)->m_right = nullptr;
+        delete (*current);
+        (*current) = tmp;
+    }
+    else if (hasLeft && !hasRight) { // has only left
+        TreeNode* tmp = (*current)->m_left;
+        (*current)->m_left = nullptr;
+        delete (*current);
+        (*current) = tmp;
+    }
+    else if (hasLeft && hasRight) { // has both
+        // go to right then left left left...
+        TreeNode** curr = &(*current)->m_right;
+        while ((*curr)->m_left != nullptr) {
+            curr = &(*curr)->m_left;
+        }
+        (*current)->m_value = (*curr)->m_value;
+        TreeNode* tmp = (*curr)->m_right;
+        (*curr)->m_right = nullptr;
+        delete (*curr);
+        (*curr) = tmp;
+    }
+    else { // has no children
+        delete (*current);
+    }
+    m_size--;
 }
 
 // Tree 
