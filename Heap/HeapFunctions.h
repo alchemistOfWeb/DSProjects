bf4e3ef196b7a30bd4f6917b642cc7b92e71ec35
Alchemist of the web <nikitakzntcv@gmail.com>
2024-12-08 13:42:44 +0400

    heap: test 1 passed

---
+++ Heap/HeapFunctions.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iterator>
+#include <functional>
+
+
+template <class RandomIt>
+void heapify(RandomIt begin, int index, size_t size);
+
+template <class RandomIt>
+void makeHeap(RandomIt begin, RandomIt end);
+
+
+template <class RandomIt>
+void heapify(RandomIt begin, int index, size_t size) {
+    int largest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < size && *(begin + left) > *(begin + largest)) {
+        largest = left;
+    }
+    if (right < size && *(begin + right) > *(begin + largest)) {
+        largest = right;
+    }
+    if (largest != index) {
+        std::swap(*(begin + index), *(begin + largest));
+        heapify(begin, largest, size);
+    }
+}
+
+
+template <class RandomIt>
+void makeHeap(RandomIt begin, RandomIt end) {
+    size_t size = std::distance(begin, end);
+    if (size <= 1) return;
+    for (int i = std::floor(size / 2) - 1; i >= 0; --i) {
+        heapify(begin, i, size);
+    }
+}
+
