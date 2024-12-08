9eef4694219de6ad107728f30fc284aacdfb6020
Alchemist of the web <nikitakzntcv@gmail.com>
2024-12-08 19:39:26 +0400

    heap: test 4 passed

--- Heap/HeapFunctions.h
+++ Heap/HeapFunctions.h
@@ -4,52 +4,100 @@
 #include <functional>
 
 
-template <class RandomIt>
-void heapify(RandomIt begin, int index, size_t size);
+template <
+    typename RandomIt,
+    typename Compare
+>
+void heapify(RandomIt begin, int index, size_t size, Compare comp);
 
-template <class RandomIt>
-void makeHeap(RandomIt begin, RandomIt end);
+
+template <
+    typename RandomIt,
+    typename Compare
+>
+void makeHeap(RandomIt begin, RandomIt end, Compare comp);
 
 
-template <class RandomIt>
-void heapify(RandomIt begin, int index, size_t size) {
+template <
+    typename RandomIt,
+    typename Compare
+>
+void popHeap(RandomIt begin, RandomIt end, Compare comp);
+
+
+template <
+    typename RandomIt,
+    typename Compare
+>
+void pushHeap(RandomIt begin, RandomIt end, Compare comp);
+
+
+
+template <
+    typename RandomIt, 
+    typename Compare = std::less<typename std::iterator_traits<RandomIt>::value_type>
+>
+void heapify(RandomIt begin, int index, size_t size, Compare comp=Compare()) {
     int largest = index;
     int left = 2 * index + 1;
     int right = 2 * index + 2;
 
-    if (left < size && *(begin + left) > *(begin + largest)) {
+    if (left < size && comp(*(begin + largest), *(begin + left))) {
         largest = left;
     }
-    if (right < size && *(begin + right) > *(begin + largest)) {
+    if (right < size && comp(*(begin + largest), *(begin + right))) {
         largest = right;
     }
     if (largest != index) {
         std::swap(*(begin + index), *(begin + largest));
-        heapify(begin, largest, size);
+        heapify(begin, largest, size, comp);
     }
 }
 
 
-template <class RandomIt>
-void makeHeap(RandomIt begin, RandomIt end) {
+template <
+    typename RandomIt,
+    typename Compare = std::less<typename std::iterator_traits<RandomIt>::value_type>
+>
+void makeHeap(RandomIt begin, RandomIt end, Compare comp=Compare()) {
     size_t size = std::distance(begin, end);
     if (size <= 1) return;
     for (int i = std::floor(size / 2) - 1; i >= 0; --i) {
-        heapify(begin, i, size);
+        heapify(begin, i, size, comp);
     }
 }
 
-template <class RandomIt>
-void popHeap(RandomIt begin, RandomIt end) {
+
+template <
+    typename RandomIt,
+    typename Compare = std::less<typename std::iterator_traits<RandomIt>::value_type>
+>
+void popHeap(RandomIt begin, RandomIt end, Compare comp=Compare()) {
     size_t size = std::distance(begin, end);
     if (size <= 1) return;
     std::iter_swap(begin, end - 1);
-    heapify(begin, 0, size - 1);
+    heapify(begin, 0, size - 1, comp);
 }
-//
-//template <class RandomIt>
-//void pushHeap(RandomIt begin, RandomIt end) {
-//    size_t size = std::distance(begin, end);
-//    if (size <= 1) return;
-//
-//}
+
+
+template <
+    typename RandomIt,
+    typename Compare = std::less<typename std::iterator_traits<RandomIt>::value_type>
+>
+void pushHeap(RandomIt begin, RandomIt end, Compare comp = Compare()) {
+    size_t size = std::distance(begin, end);
+    if (size <= 1) return;
+    int newIndex = size - 1;
+    while (newIndex > 0) {
+        int parentIndex = (newIndex-1) / 2;
+        auto& parentEl = *(begin + parentIndex);
+        auto& newEl = *(begin + newIndex);
+        if (comp(parentEl, newEl)) {
+            std::swap(parentEl, newEl);
+            newIndex = parentIndex;
+        }
+        else {
+            break;
+        }
+    }
+}
--- Heap/main.cpp
+++ Heap/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "Tests/1MakeHeapTest.h"
 #include "Tests/2PopHeapTest.h"
-//#include "Tests/3PushHeapTest.h"
-//#include "Tests/4ComparatorTest.h"
+#include "Tests/3PushHeapTest.h"
+#include "Tests/4ComparatorTest.h"
 //#include "Tests/5ComparisonCounterTest.h"
 
 int main() 
